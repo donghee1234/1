2d520b3557b977ca16e1c7fd1e8c6ff8ff273dbf
donghee1234 <127944467+donghee1234@users.noreply.github.com>
2026-04-07 22:49:23 +0900

    Create arrpoint.c

---
+++ arrpoint.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int N;
+    int *arr;
+    int sum = 0, max, min;
+
+    printf("배열의 크기를 입력하세요: ");
+    scanf("%d", &N);
+
+    arr = (int *)malloc(sizeof(int) * N);
+
+    srand(time(NULL));
+    for (int i = 0; i < N; i++) {
+        *(arr + i) = rand() % (N + 1);
+    }
+
+    printf("처음부터 출력: ");
+    for (int i = 0; i < N; i++) {
+        printf("%d ", *(arr + i));
+    }
+    printf("\n");
+
+    printf("뒤에서부터 출력: ");
+    for (int i = N - 1; i >= 0; i--) {
+        printf("%d ", *(arr + i));
+    }
+    printf("\n");
+
+    max = min = *arr;
+    for (int i = 0; i < N; i++) {
+        int current = *(arr + i);
+        sum += current;
+        
+        if (current > max) max = current;
+        if (current < min) min = current;
+    }
+
+    printf("총합: %d / 최대값: %d / 최소값: %d\n", sum, max, min);
+
+    free(arr);
+
+    return 0;
+}
