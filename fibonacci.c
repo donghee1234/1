a3ae46771a90a276859beeefde797af93f564b68
donghee1234 <127944467+donghee1234@users.noreply.github.com>
2026-04-14 21:38:01 +0900

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <time.h>
 
@@ -12,11 +13,11 @@ long long fibo_iterative(int n) {
     if (n <= 2) {
         return 1;
     }
-    
+
     long long prev = 1; // F(n-2)
     long long curr = 1; // F(n-1)
     long long next = 0;
-    
+
     for (int i = 3; i <= n; i++) {
         next = prev + curr;
         prev = curr;
@@ -30,7 +31,7 @@ int main() {
     clock_t start, end;
     double duration;
 
-    printf("수열 N 값 입력: ");
+    printf("수열  값: ");
     scanf("%d", &n);
 
     printf("\n--- 결과 ---\n");
@@ -39,14 +40,13 @@ int main() {
     long long res1 = fibo_iterative(n);
     end = clock();
     duration = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("순환 방식: %lld (걸린시간: %f 초)\n", res1, duration);
+    printf("순환: F(%d) = %lld (시간: %f 초)\n", n, res1, duration);
 
     printf("재귀 방식\n");
     start = clock();
     long long res2 = fibo_recursive(n);
     end = clock();
     duration = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("재귀 방식 결과: %lld (걸린시간: %f 초)\n", res2, duration);
-
+    printf("재귀: F(%d) = %lld (시간: %f 초)\n", n, res2, duration);
     return 0;
 }
